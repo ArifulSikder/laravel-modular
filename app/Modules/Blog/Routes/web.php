54532947ad09732b595c0a2d0adfeac6cee18bc9
M <mdhannan.info@gmail.com>
2023-09-09 11:02:24 +0600

    Added route

--- app/Modules/Blog/Routes/web.php
+++ app/Modules/Blog/Routes/web.php
@@ -2,4 +2,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Modules\Blog\Controllers\BlogController;
 
-Route::resource('blogs', BlogController::class);
+Route::controller(BlogController::class)->prefix('blog')->name('blog.')->group(function () {
+    Route::get('index', 'index')->name('index');
+    Route::get('edit/{id}', 'edit')->name('edit');
+    Route::any('update/{id}', 'updatePassword')->name('update');
+    Route::delete('delete/{id}', 'delete')->name('delete');
+});
