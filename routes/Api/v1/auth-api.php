04b2e0858f83f4aa043c4c80a78af3b44fabf047
ArifulSikder <mdislamnoyon3@gmail.com>
2023-09-24 22:50:05 +0600

    Remove Command

--- routes/Api/v1/auth-api.php
+++ routes/Api/v1/auth-api.php
@@ -4,16 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Acl\AuthController;
 use App\Http\Controllers\Acl\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 // Route::group(['middleware' => ['cors', 'json.response']], function () {
     Route::controller(AuthController::class)->group(function () {
