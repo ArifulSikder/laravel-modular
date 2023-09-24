75df8d7820ad60e38dc42b040a9e658a1dc3d68d
Muhammad Hannan <mdhannan.info@gmail.com>
2023-09-24 17:00:00 +0600

    Login api done

--- app/Http/Controllers/Acl/AuthController.php
+++ app/Http/Controllers/Acl/AuthController.php
@@ -6,7 +6,9 @@ use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Response;
 use App\Http\Repositories\UserRepository;
 use Illuminate\Support\Facades\Validator;
 
@@ -42,7 +44,13 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-       dd('asdf');
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $result = $user->createToken('MyApp')->accessToken;
+            return apiSuccessResponse($result, 'Logged in successfully');
+        }
+        return response()->json(['error' => 'Unauthorized'], 401);
     }
 
 }
